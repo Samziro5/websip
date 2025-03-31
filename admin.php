2fc51fc4f7db611b4596e8a2fdb416cf3bbe0678
Samziro5 <samzirooficial@gmail.com>
2025-03-31 10:07:58 -0600

    4 botones + 3 botnoes funcionando

--- admin.php
+++ admin.php
@@ -68,29 +68,28 @@ header("Location:login.php");
         </section>   
     </header>    
     <section class="seccion-btn-group">
-        <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+        <form>
+        
+                <div class="btn-group" role="group" aria-label="Basic radio toggle button group">
+            
+            
+                <input type="radio" class="btn-check" name="group" id="btnradio1" autocomplete="off" checked>
+                <label class="btn btn-outline-primary" for="btnradio1" onclick="showSection('infracciones')">Total General de Infracciones a Vehículos</label>
+                
+                <input type="radio" class="btn-check" name="group" id="btnradio2" autocomplete="off">
+                <label class="btn btn-outline-primary" for="btnradio2" onclick="showSection('motocicletas')">Total General de Infracciones a Motocicletas</label>
 
-        <input type="radio" class="btn-check" name="btnradio" id="btnradio1" autocomplete="off" checked>
-        <label class="btn btn-outline-primary" for="btnradio1" onclick="showSection('infracciones')">Infracciones</label>
-   
+                <input type="radio" class="btn-check" name="group" id="btnradio3" autocomplete="off">
+                <label class="btn btn-outline-primary" for="btnradio3" onclick="showSection('depositos')">Total General Depositos</label>
 
-        <input type="radio" class="btn-check" name="btnradio" id="btnradio2" autocomplete="off">
-        <label class="btn btn-outline-primary" for="btnradio2" onclick="showSection('motocicletas')">Motocicletas</label>
-
-        <input type="radio" class="btn-check" name="btnradio" id="btnradio3" autocomplete="off">
-        <label class="btn btn-outline-primary" for="btnradio3" onclick="showSection('depositos')">Depositos</label>
-
-        <input type="radio" class="btn-check" name="btnradio" id="btnradio4" autocomplete="off">
-        <label class="btn btn-outline-primary" for="btnradio4" onclick="showSection('fotos-civicas')">Fotos Civicas</label>
-
-        <input type="radio" class="btn-check" name="btnradio" id="btnradio5" autocomplete="off">
-        <label class="btn btn-outline-primary" for="btnradio5" onclick="showSection('total-general')">Total Generales</label>
-        </div>
-
-    </section>
-    
+                <input type="radio" class="btn-check" name="group" id="btnradio4" autocomplete="off">
+                <label class="btn btn-outline-primary" for="btnradio4" onclick="showSection('fotos-civicas')">Tota General Fotos Civicas</label>
+                </div>
+                
+        </form>
+</section>
         <section id="infracciones" class="info-section">
-                <?php include 'prueba1.html';?>
+                <?php include 'prueba1.php';?>
         </section>
         <section id="motocicletas" class="info-section"style="display:none;">
                 <?php include 'prueba2.html';?>
@@ -101,11 +100,9 @@ header("Location:login.php");
         <section id="fotos-civicas" class="info-section" style="display:none;">
             <?php include 'prueba4.html';?>
         </section>
-        <section id="total-general" class="info-section" style="display:none;">
-                <?php include 'prueba5.html';?>
-        </section>
-
-    <script>
+     
+    
+     <script>
         function showSection(sectionId) {
         var sections = document.querySelectorAll('.info-section');
         sections.forEach(function(section) {
@@ -116,10 +113,8 @@ header("Location:login.php");
             selectedSection.style.display = 'block';
             }
         }
-    </script>
-    
-
-   
+    </script> 
+      
     
 </body>
 
@@ -134,10 +129,3 @@ header("Location:login.php");
     
 </footer>
 </html>
-<?php
-// logout.php
-session_start();
-session_destroy(); // Destruye la sesión
-header("Location: login.php"); // Redirige a la página de inicio de sesión
-exit();
-?>
---
+++ prueba1.php
@@ -0,0 +1,56 @@
+<h1>infracciones</h1>
+<section class="seccion-btn-group2">
+    
+    <form>
+       
+                    <div class="btn-group2" role="group2" aria-label="Basic radio toggle button group2">
+            
+                    <input type="radio" class="btn-check" name="group2" id="btnradio5" autocomplete="off" checked>
+                    <label class="btn btn-outline-primary" for="btnradio5" onclick="showSection2('Anos')">Años</label>
+                                
+                    <input type="radio" class="btn-check" name="group2" id="btnradio6" autocomplete="off">
+                    <label class="btn btn-outline-primary" for="btnradio6" onclick="showSection2('Meses')">Meses</label>
+            
+                    <input type="radio" class="btn-check" name="group2" id="btnradio7" autocomplete="off">
+                    <label class="btn btn-outline-primary" for="btnradio7" onclick="showSection2('Dias')">Dias</label>
+                    </div> 
+      
+    </form>
+</section>
+
+
+        <section id="Anos" class="info-section2" >
+                <?php include 'inf_ano.html';?> 
+        </section>
+        <section id="Meses" class="info-section2"style="display:none;">
+                <?php include 'inf_mes.html';?>
+        </section>
+        <section id="Dias" class="info-section2" style="display:none;">
+                <?php include 'inf_dias.html';?>
+        </section>
+
+ <script>
+    // Mostrar los botones secundarios (Grupo 2)
+    function showGroup2() {
+        var group2 = document.getElementById('group2');
+        group2.style.display = 'block';
+    }
+
+    // Mostrar la sección seleccionada
+    function showSection2(sectionId) {
+        // Ocultar todas las secciones
+        var sections = document.querySelectorAll('.info-section2');
+        sections.forEach(function(section) {
+            section.style.display = 'none';
+        });
+
+        // Mostrar la sección seleccionada
+        var selectedSection = document.getElementById(sectionId);
+        if (selectedSection) {
+            selectedSection.style.display = 'block';
+        }
+    }
+    </script> 
+    
+
+                <!-- Medidas del Grafico width="100%" height="1100"  -->
